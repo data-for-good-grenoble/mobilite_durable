9c2f88448df9b12b8d040e1c704cf2daba88971e
NicolasGrosjean <grosjen@hotmail.fr>
2025-09-04 20:25:20 +0200

    chore: log progress

--- src/scrapping/scrapping/spiders/transit.py
+++ src/scrapping/scrapping/spiders/transit.py
@@ -15,10 +15,14 @@ class TransitSpider(scrapy.Spider):
     start_urls = [
         "https://transitapp.com/fr/region/grenoble/ara-cars-r%C3%A9gion-is%C3%A8re-scolaire",
     ]
+    processing_bus_line_number = 0
+    total_bus_line_number = 0
 
     def parse(self, response: Response):
         # Search lines with the class "padding-route-image"
         bus_line_paths = response.xpath('//div[contains(@class, "padding-route-image")]')
+        self.total_bus_line_number = len(bus_line_paths)
+        self.logger.info(f"Found {self.total_bus_line_number} bus lines")
         for bus_line_path in bus_line_paths:
             # Extract texts form the containing span
             texts = bus_line_path.xpath("..//text()").extract()
@@ -39,6 +43,8 @@ class TransitSpider(scrapy.Spider):
                 )
 
     def parse_bus_line(self, response: Response):
+        self.logger.info(f"Processing bus line {self.processing_bus_line_number + 1}/{self.total_bus_line_number}")
+        self.processing_bus_line_number += 1
         bus_line_number = response.meta.get("bus_line_number")
         bus_line_name = response.meta.get("bus_line_name")
 
