566e9795e55045818881d633faaff1436db381e3
NicolasGrosjean <grosjen@hotmail.fr>
2025-09-04 21:04:41 +0200

    chore: improve transit spider

--- src/scrapping/scrapping/spiders/transit.py
+++ src/scrapping/scrapping/spiders/transit.py
@@ -29,31 +29,42 @@ class TransitSpider(scrapy.Spider):
             bus_line_number = texts[0] if len(texts) > 0 else ""
             bus_line_name = texts[1] if len(texts) > 1 else ""
 
-            # The link is included in the parent of the parent of the parent
-            bus_line_url = bus_line_path.xpath("../../..//@href").get()
-            if bus_line_url:
+            # The link is included in the first <a> parent element
+            relative_bus_line_url = bus_line_path.xpath("ancestor::a[1]//@href").get()
+            if relative_bus_line_url:
+                full_bus_line_url = f"https://transitapp.com/{relative_bus_line_url}"
                 yield response.follow(
-                    f"https://transitapp.com/{bus_line_url}",
+                    full_bus_line_url,
                     callback=self.parse_bus_line,
                     meta={
                         "bus_line_number": bus_line_number,
                         "bus_line_name": bus_line_name,
-                        "bus_line_url": bus_line_url,
+                        "bus_line_url": full_bus_line_url,
                     },
                 )
+            else:
+                self.logger.warning(f"No URL found for bus line {bus_line_number}")
+                yield {
+                    "bus_line_number": bus_line_number,
+                    "bus_line_name": bus_line_name,
+                    "bus_line_url": "",
+                    "stop_names": [],
+                }
 
     def parse_bus_line(self, response: Response):
         self.logger.info(f"Processing bus line {self.processing_bus_line_number + 1}/{self.total_bus_line_number}")
         self.processing_bus_line_number += 1
         bus_line_number = response.meta.get("bus_line_number")
         bus_line_name = response.meta.get("bus_line_name")
+        bus_line_url = response.meta.get("bus_line_url")
 
-        # Search stops with color-black-pearl class
-        stop_names = response.xpath(
-            '//span[contains(@class, "color-black-pearl")]//text()'
-        ).extract()
+
+        # Search stops with RouteStationsList class
+        stop_and_hours = response.xpath('//div[contains(@class, "RouteStationsList")]//text()').extract()
+        stop_names = stop_and_hours[::2]
         yield {
             "bus_line_number": bus_line_number,
             "bus_line_name": bus_line_name,
+            "bus_line_url": bus_line_url,
             "stop_names": stop_names,
         }
