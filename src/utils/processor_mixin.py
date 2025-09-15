a60d744f79e9c390323794573784ab5badae9d76
Riws <t.debruyne@protonmail.com>
2025-09-15 19:17:29 +0200

    feat: ProcessorMixin v2

--- src/utils/processor_mixin.py
+++ src/utils/processor_mixin.py
@@ -1,10 +1,13 @@
+import logging
 from pathlib import Path
 from typing import Any, Type
+
+logger = logging.getLogger(__name__)
 
 
 class ProcessorMixin:
     """
-    api_class va permettre d'aller récupérer la donnée en ligne
+    api_class est la classe de l'API
     input_file va permettre de trouver le fichier d'entrée, sauvegarde non modifiée de l'api_class
     output_file va permettre de trouver le fichier de sortie, sauvegarde modifiée de l'input_file
     """
@@ -19,57 +22,91 @@ class ProcessorMixin:
         raise Exception("Utility class")
 
     @classmethod
-    def run(cls, reload_pipeline: bool = False) -> None:
-        content = cls.fetch(reload_pipeline=reload_pipeline)
-
-        if cls.output_file and (reload_pipeline or not cls.output_file.exists()):
-            cls.save(content, cls.output_file)
+    def run(
+        cls,
+        reload_pipeline: bool = False,
+        content: Any = None,
+        fetch_api_kwargs: dict | None = None,
+        fetch_input_kwargs: dict | None = None,
+        fetch_output_kwargs: dict | None = None,
+    ) -> None:
+        content = cls.fetch(
+            reload_pipeline=reload_pipeline,
+            content=None,
+            fetch_api_kwargs=fetch_api_kwargs,
+            fetch_input_kwargs=fetch_input_kwargs,
+            fetch_output_kwargs=fetch_output_kwargs,
+        )
+        if content is None:
+            logger.warning(f"{cls.__name__}: have no content")
 
     @classmethod
-    def fetch(cls, reload_pipeline: bool = False) -> Any:
+    def fetch(
+        cls,
+        reload_pipeline: bool = False,
+        content: Any = None,
+        fetch_api_kwargs: dict | None = None,
+        fetch_input_kwargs: dict | None = None,
+        fetch_output_kwargs: dict | None = None,
+    ) -> Any:
         """
         Récupère la donnée et la sauvegarde si besoin
         Il existe 3 niveaux d'informations : celle de l'api, celle de l'input_file et celle de l'output_file
         Lors d'un process où `reload_pipeline` is False (cas par défaut) :
             - on va regarder l'output_file, s'il n'existe pas ou s'il n'est pas configuré
             - on va regarder l'input_file, que l'on va procésser, s'il n'existe pas ou s'il n'est pas configuré
-            - on va regarder l'api_class, pour télécharger la donnée et la sauvegarder, s'il n'existe pas, une exception est levée
+            - on va regarder l'api_class, pour télécharger la donnée et la sauvegarder
         Lors d'un process où `reload_pipeline` is True, le process est inversé
         """
+        fetch_api_kwargs = fetch_api_kwargs or dict()
+        fetch_input_kwargs = fetch_input_kwargs or dict()
+        fetch_output_kwargs = fetch_output_kwargs or dict()
 
-        # TODO: fetch_from_api si le fichier de l'url a été mis à jour
-        def fetch_from_api():
-            if cls.api_class:
-                content = cls.fetch_from_api()
-                if cls.input_file:
-                    cls.save(content, cls.input_file)
-                return fetch_from_loaded_input_file(content)
+        if reload_pipeline:
+            api_content = cls.fetch_and_save_from_api(content, **fetch_api_kwargs)
+            input_content = cls.fetch_and_save_from_input_file(
+                api_content, **fetch_input_kwargs
+            )
+            output_content = cls.fetch_from_output_file(input_content, **fetch_output_kwargs)
+        else:
+            output_content = cls.fetch_from_output_file(**fetch_output_kwargs)
+            if output_content is None:
+                input_content = cls.fetch_and_save_from_input_file(**fetch_input_kwargs)
+                if input_content is None:
+                    api_content = cls.fetch_and_save_from_api(**fetch_api_kwargs)
+                    input_content = cls.fetch_and_save_from_input_file(
+                        api_content, **fetch_input_kwargs
+                    )
+                output_content = cls.fetch_from_output_file(
+                    input_content, **fetch_output_kwargs
+                )
+        return output_content
 
-        def fetch_from_loaded_input_file(content):
-            processed = cls.pre_process(content)
-            if cls.output_file:
-                cls.save(processed, cls.output_file)
-            return processed
+    @classmethod
+    def fetch_and_save_from_api(cls, content=None, save: bool = True):
+        if content is None and cls.api_class:
+            content = cls.fetch_from_api()
 
-        def fetch_from_input_file():
-            if cls.input_file and cls.input_file.exists():
-                content = cls.fetch_from_file(cls.input_file)
-                return fetch_from_loaded_input_file(content)
+        if save and cls.input_file:
+            cls.save(content, cls.input_file)
+        return content
 
-        def fetch_from_output_file():
-            if cls.output_file and cls.output_file.exists():
-                return cls.fetch_from_file(cls.output_file)
+    @classmethod
+    def fetch_and_save_from_input_file(cls, content=None, save: bool = True):
+        if content is None and cls.input_file:
+            content = cls.fetch_from_file(cls.input_file)
 
-        methods = [fetch_from_api, fetch_from_input_file, fetch_from_output_file]
-        if not reload_pipeline:
-            methods = methods[::-1]
+        processed = cls.pre_process(content)
+        if save and cls.output_file:
+            cls.save(processed, cls.output_file)
+        return processed
 
-        for method in methods:
-            content = method()
-            if content is not None:
-                return content
+    @classmethod
+    def fetch_from_output_file(cls, content=None):
+        if content is None and cls.output_file:
+            content = cls.fetch_from_file(cls.output_file)
 
-        raise Exception
+        return cls.post_process(content)
 
     @classmethod
     def fetch_from_api(cls, **kwargs):
@@ -77,12 +114,23 @@ class ProcessorMixin:
 
     @classmethod
     def fetch_from_file(cls, path: Path, **kwargs):
-        raise NotImplementedError
+        if not path.exists():
+            logger.warning(f"{cls.__name__}: {path} does not exist")
+            return
+        # TODO: logic
 
     @classmethod
     def save(cls, content, path: Path) -> None:
+        if content is None:
+            logger.error(f"{cls.__name__}: cannot save because `content` attribute is None")
+            return
         path.parent.mkdir(parents=True, exist_ok=True)
+        # TODO: logic
 
     @classmethod
     def pre_process(cls, content, **kwargs):
+        return content
+
+    @classmethod
+    def post_process(cls, content, **kwargs):
         return content
